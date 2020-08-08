0c55d4e4345246cad5d25a456e216bf4fbfbbea8
paddison <patrickbaumann579@gmail.com>
2020-08-08 18:51:13 +0200

    added some styling to view page

--- auctions/forms.py
+++ auctions/forms.py
@@ -9,12 +9,12 @@ class AuctionForm(forms.Form):
     picture = forms.URLField(label="Link a URL of a picture:", required=False)
 
 class BidForm(forms.Form):
-    placed_bid = forms.DecimalField(label="Bid:", max_digits=7, decimal_places=2)
+    placed_bid = forms.DecimalField(max_digits=7, decimal_places=2)
 
 class WatchlistForm(forms.Form):
     auction_id = forms.IntegerField(widget=forms.HiddenInput())
     onWatchlist = forms.BooleanField(widget=forms.HiddenInput(), required=False)
 
 class CommentForm(forms.Form):
-    comment = forms.CharField(label="Post a comment", widget=forms.Textarea, max_length=256)
+    comment = forms.CharField(label="Post a comment", widget=forms.Textarea(attrs={'class': 'view-comment-textarea'}), max_length=256)
     auction_id = forms.IntegerField(widget=forms.HiddenInput())
